a7947c27c7273cf787d6f1348ef9d48caffbc6a2
Ubuntu <ubuntu@ip-10-0-7-147.ec2.internal>
2025-11-01 13:56:51 +0000

    Intial commit -L3 CI/CD

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html;charset=UTF-8");
+    resp.getWriter().write("<h1>Hello from EC2 Maven WebApp!</h1>");
+  }
+}
